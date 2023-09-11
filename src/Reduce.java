6f37525c3ca9185c8846a067129eb6cc87ad7e24
seth0 <seth0712@gmail.com>
2023-09-11 18:13:59 -0400

    Added reduce method

---
+++ src/Reduce.java
@@ -0,0 +1,13 @@
+public class Reduce {
+    public static void main(String[] args) {
+        int i = 100;
+        while (i > 0){
+            if (i % 2 == 0){
+                i = i / 2;
+            }
+            else {
+                i--;
+            }
+        }
+    }
+}
