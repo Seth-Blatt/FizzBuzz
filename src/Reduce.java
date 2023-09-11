0f7a80cbeba429ce222168830ab4759ed1b294da
seth0 <seth0712@gmail.com>
2023-09-11 18:15:15 -0400

    Fixed reduce method

--- src/Reduce.java
+++ src/Reduce.java
@@ -1,6 +1,7 @@
 public class Reduce {
     public static void main(String[] args) {
         int i = 100;
+        int j = 0;
         while (i > 0){
             if (i % 2 == 0){
                 i = i / 2;
@@ -8,6 +9,8 @@ public class Reduce {
             else {
                 i--;
             }
+            j++;
         }
+        System.out.println(j);
     }
 }
